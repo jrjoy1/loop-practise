2661917db25b9064ce72324a43a1c5045a6150a9
jrjoy1 <joy2244680@gmail.com>
2024-02-14 15:37:01 +0600

    added loop practice files

---
+++ break/1-century.js
@@ -0,0 +1,9 @@
+let num = 1;
+
+while (num <= 200) {
+    console.log(num);
+    num++;
+    if(num > 100){
+        break;
+    }
+}
---
+++ break/2sum-upto100.js
@@ -0,0 +1,9 @@
+let num = 1;
+
+while (num <= 100) {
+    console.log(num);
+    num++;
+    if(num > 100){
+        break;
+    }
+}
